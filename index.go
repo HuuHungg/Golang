a1064a213232b1e1d6742033326bcff63abcba15
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-02 12:24:46 +0700

    add file modifle

--- index.go
+++ index.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 )
 
+// Kiểu dữ liệu khai báo Enum tập hợp nhiều hằng số lại với nhau
+
+const (
+	// iota chỉ sử dụng trong một block 
+	red = iota
+	yellow 
+	blue 
+	black 
+)
+const (
+	// iota chỉ sử dụng trong một block 
+	red1 = iota + 5
+	yellow1 
+
+)
+
+
 func main() {
-	// Text
-	// Byte -> UTF-8
-	// Rune -> UTF-32
+	const i = "Hello"
+	const a = 12
+	fmt.Printf("%v %T\n", i, i)
+	fmt.Printf("%v %T\n", a, a)
 
-	var a string = "こんにちは世界"
-	var b []byte = []byte(a)
-	fmt.Printf("%v %T\n", b, b)
-	
-	// Chuyển kiểu
-	var c byte = 'H'
-	fmt.Printf("%v, %T\n", string(c), c)
-
-	var d rune = '世'
-	fmt.Printf("%v, %T\n", string(d), d)
+	fmt.Printf("%v %T\n", red, red)
+	fmt.Printf("%v %T\n", yellow, yellow)
+	fmt.Printf("%v %T\n", blue, blue)
+	fmt.Printf("%v %T\n", black, black)
+	fmt.Printf("%v %T\n", red1, red1)
+	fmt.Printf("%v %T\n", yellow1, yellow1)
 }
 
