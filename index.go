f38e9972ac8b349d7e2fb416d91ee472339ce2a4
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-02 14:30:54 +0700

    add file modifile

--- index.go
+++ index.go
@@ -1,37 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
-
-// Kiểu dữ liệu khai báo Enum tập hợp nhiều hằng số lại với nhau
-
-const (
-	// iota chỉ sử dụng trong một block 
-	red = iota
-	yellow 
-	blue 
-	black 
-)
-const (
-	// iota chỉ sử dụng trong một block 
-	red1 = iota + 5
-	yellow1 
-
-)
-
-
-func main() {
-	const i = "Hello"
-	const a = 12
-	fmt.Printf("%v %T\n", i, i)
-	fmt.Printf("%v %T\n", a, a)
-
-	fmt.Printf("%v %T\n", red, red)
-	fmt.Printf("%v %T\n", yellow, yellow)
-	fmt.Printf("%v %T\n", blue, blue)
-	fmt.Printf("%v %T\n", black, black)
-	fmt.Printf("%v %T\n", red1, red1)
-	fmt.Printf("%v %T\n", yellow1, yellow1)
+	import (
+		"fmt"
+	)
+func main()  {
+	// points := [...]int {7,10,5,6}
+	// fmt.Printf("%v %T", points, points)
+	
+	var course [5]string
+	course[0] = "alex"
+	course[1] = "Tom"
+	course[2] = "Yuh"
+	course[3] = "Space"
+	
+	fmt.Printf("%v %T\n", course, course)
+	fmt.Printf("%v %T\n", course[2], course)
+	fmt.Println(len(course))
+	
 }
 
