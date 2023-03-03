324707e5486357058da86f7aea554c90851875a0
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-03 10:41:32 +0700

    add file modifile

--- index.go
+++ index.go
@@ -3,20 +3,29 @@ package main
 	import (
 		"fmt"
 	)
-func main()  {
-	// slice 
-	a := []int{2,5,10,40,50,60,80,90}
-	b := a[:] // hai dấu chấm  đứng một mình là lấy hết các phần tử của a
-	c := a[3:] // con trỏ bắt đầu từ index vị trí số 3
-	d := a[:5] 
-	e := a[3:5]
-	fmt.Printf("a %v, %v, %v\n", a, len(a),cap(a))	
-	fmt.Printf("b %v, %v, %v\n", b, len(b),cap(b))	
-	fmt.Printf("c %v, %v, %v\n", c, len(c),cap(c))	
-	fmt.Printf("d %v, %v, %v\n", d, len(d),cap(d))	
-	fmt.Printf("e %v, %v, %v\n", e, len(e),cap(e))	
 
+func main () {
+	number := 90
+	guess := 85
+	
+	if !(number < 10 || !false || guess > 100) {
+		fmt.Println("Con số bạn đoán nhỏ hơn 10 hoặc là nhà hơn 100")
+	} else {
+		if guess < number {
+			fmt.Println("Con so muc tieu lon hon con so ban dau")
+		} else if guess > number {
+			fmt.Println("Con so muc tieu nho hon con so ban dau")
+		} else guess == number {
+			fmt.Println("Ban da doan dung")
+		}
+	}
+
+
+	
 }
+
+
+
 
 
 
