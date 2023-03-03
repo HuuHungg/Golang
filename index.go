8434181b5d591b0e56a887a966b7119b9c5e6b71
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-03 10:59:04 +0700

    add file modfifile

--- index.go
+++ index.go
@@ -5,22 +5,31 @@ package main
 	)
 
 func main () {
-	number := 90
-	guess := 85
-	
-	if !(number < 10 || !false || guess > 100) {
-		fmt.Println("Con số bạn đoán nhỏ hơn 10 hoặc là nhà hơn 100")
-	} else {
-		if guess < number {
-			fmt.Println("Con so muc tieu lon hon con so ban dau")
-		} else if guess > number {
-			fmt.Println("Con so muc tieu nho hon con so ban dau")
-		} else guess == number {
-			fmt.Println("Ban da doan dung")
-		}
+	number := 2
+	switch  {
+		case number <= 3:
+			fmt.Println("Nho hon hoac bang 3")
+			fallthrough
+		case number <=5:
+			fmt.Println("Nho hon hoac bang 5")
+			fmt.Println("Nho hon hoac bang 5")
+			break
+			fmt.Println("Nho hon hoac bang 5")
+			fallthrough // kết tiếp
+		default:
+			fmt.Println("Khong thuoc truong hop nao")
 	}
 
-
+		var i interface{} = 34.5
+		switch i.(type) {
+			case int:
+					fmt.Println("Kieu du lieu int")
+			case float64:
+					fmt.Println("Kieu du lieu float64")
+			default:
+					fmt.Println("Khong biet kieu du lieu")
+		}
+	
 	
 }
 
